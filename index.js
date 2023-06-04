4f8788c8c4c95e651f4536be356ef647fa30589d
Jatin Gupta <jatin71544@gmail.com>
2023-06-04 23:17:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function checkfunction(){
-  if("2000-07-02"<DATE<"2005-07-01"){
+  if(2000-07-02 <DATE< 2005-07-01){
     document.getElementById("goresult").innerHTML="Eligible For CDS 1 2023 (IMA, AFA, INA, OTA); Note: attempt limit for candidates holding Commercial Pilot Licence issued by DGCA (India) is +2 years (till 26 years)";
   }
   else{
