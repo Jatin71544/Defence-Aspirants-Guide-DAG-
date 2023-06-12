69974b142cec616451b43de7de349a14f8d5c9fe
Jatin Gupta <jatin71544@gmail.com>
2023-06-12 10:54:05 +0530

    adding script.js file

---
+++ script.js
@@ -0,0 +1,18 @@
+function checkEligibility() {
+  // Retrieve the input value
+  var dateInput = document.getElementById('dateInput').value;
+
+  // Convert the input value to a Date object
+  var inputDate = new Date(dateInput);
+
+  // Define the eligibility criteria dates
+  var minEligibleDate = new Date('2000-07-02');
+  var maxEligibleDate = new Date('2005-07-01');
+
+  // Check if the input date is within the eligibility range
+  if (inputDate >= minEligibleDate && inputDate <= maxEligibleDate) {
+    document.getElementById('result').textContent = 'You are eligible! Note: (Upper age limit for candidates holding valid and current Commercial Pilot Licence issued by DGCA (India) is relaxable upto 26 yrs,in short +2 years or attempts.';
+  } else {
+    document.getElementById('result').textContent = 'Sorry, You are not eligible.';
+  }
+}
